ba795b0517a208497a0f6d885d32001cdd9e0e77
Aman Mittal <amandeepmittal@live.com>
2018-07-11 13:17:51 +0530

    refactor: custom component

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import {
-	StyleSheet,
-	TextInput,
-	KeyboardAvoidingView,
-	View
-} from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, View } from 'react-native';
 import CardStackView from 'react-native-cardstack-view';
 
+import SearchInput from './components/SearchInput';
 import WeatherText from './components/WeatherText';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<KeyboardAvoidingView style={styles.container} behavior="padding">
-				<View style={styles.searchBar}>
-					<TextInput
-						autoCorrect={false}
-						placeholder="Search any City"
-						placeholderTextColor="#ffffff"
-						style={styles.textInput}
-						clearButtonMode="always"
-					/>
-				</View>
+				<SearchInput />
 				<View style={styles.cards}>
 					<CardStackView backgroundColor="#ebebeb">
 						<WeatherText />
@@ -36,20 +24,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff'
-	},
-	searchBar: {
-		flex: 1,
-		paddingTop: 50
-	},
-	textInput: {
-		width: 300,
-		height: 50,
-		marginTop: 40,
-		backgroundColor: '#666666',
-		marginHorizontal: 20,
-		paddingHorizontal: 10,
-		alignSelf: 'center',
-		color: '#ffffff'
 	},
 	cards: {
 		flex: 2
---
+++ components/SearchInput.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { TextInput, StyleSheet, View } from 'react-native';
+
+class SeachInput extends Component {
+	render() {
+		return (
+			<View style={styles.searchBar}>
+				<TextInput
+					autoCorrect={false}
+					placeholder="Search any City"
+					placeholderTextColor="#ffffff"
+					style={styles.textInput}
+					clearButtonMode="always"
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	searchBar: {
+		flex: 1,
+		paddingTop: 50
+	},
+	textInput: {
+		width: 300,
+		height: 50,
+		marginTop: 40,
+		backgroundColor: '#666666',
+		marginHorizontal: 20,
+		paddingHorizontal: 10,
+		alignSelf: 'center',
+		color: '#ffffff'
+	}
+});
+
+export default SeachInput;
